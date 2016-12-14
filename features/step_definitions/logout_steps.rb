f451a5628891d72416b4d1327560305c2555ed63
Amber <amber@amberwilkie.com>
2016-12-14 16:20:16 +0100

    Install FactoryGirl and add spec for user

--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,12 +1,12 @@
 Given(/^that there is a user named "([^"]*)"$/) do |name|
-  user = User.create(name: name, email: "user@user.com", password: "12345678")
+  # user = User.create(name: name, email: "user@user.com", password: "12345678")
+  user = FactoryGirl.create(:user, name: name)
 end
 
 Given(/^"([^"]*)" is logged in$/) do |name|
   user = User.find_by(name: name)
   login_as(user, scope: :user)
 end
-
 
 Given(/^I am on the "([^"]*)" page$/) do |page|
   if page == "inbox"
