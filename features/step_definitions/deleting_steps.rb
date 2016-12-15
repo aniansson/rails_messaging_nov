c91a91b5cd657ec1a4f262d57e4fb866b8c8c1b5
blondabambi <ebba@aniansson.se>
2016-12-15 16:18:22 +0100

    create steps file

---
+++ features/step_definitions/deleting_steps.rb
@@ -0,0 +1,41 @@
+Given(/^there is users named:$/) do |table|
+  table.hashes.each do |hashes|
+  FactoryGirl.create(hash)
+  end
+end
+
+# Given(/^I am logged in as "([^"]*)"$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Given(/^I send an email to "([^"]*)"$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Given(/^I am on the "([^"]*)"$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Given(/^I click on "([^"]*)"$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Given(/^that I am logged in as "([^"]*)"$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Given(/^I click on the "([^"]*)" page$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then(/^I should have "([^"]*)" message$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then(/^I click on the "([^"]*)" link$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then(/^I should have "([^"]*)" messages$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
